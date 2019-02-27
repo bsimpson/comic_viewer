aa99a4efd12451d4a2af541175ca5ec229c6d12a
Ben Simpson <thehoagie@gmail.com>
2019-02-27 08:48:26 -0500

    Fix image width in donthitsave

--- server/adapters/donthitsave.js
+++ server/adapters/donthitsave.js
@@ -1,7 +1,19 @@
 const utils = require('../utils');
+const cheerio = require('cheerio');
 
 const URL = 'http://donthitsave.com/donthitsavefeed.xml';
 
 module.exports = {
-  load: () => utils.parseFeed(URL),
+  load: async () => {
+    const parsedFeed = await utils.parseFeed(URL);
+
+    return Promise.all(parsedFeed.items.map((item) => {
+      const $ = cheerio.load(item.content);
+
+      const imgSrc = $('img').attr('src');
+      item.content = `<img src="${imgSrc}" class="img-fluid" />`;
+
+      return item;
+    })).then(() => parsedFeed);
+  }
 };
