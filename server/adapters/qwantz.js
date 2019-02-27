90c07f6ed4cb419fbdd29c60adc383bc361f2553
Ben Simpson <thehoagie@gmail.com>
2019-02-27 08:41:37 -0500

    Fix qwantz formatting

--- server/adapters/qwantz.js
+++ server/adapters/qwantz.js
@@ -1,6 +1,18 @@
 const utils = require('../utils');
 const URL = 'http://qwantz.com/rssfeed.php';
+const cheerio = require('cheerio');
 
 module.exports = {
-  load: () => utils.parseFeed(URL),
+  load: async () => {
+    const parsedFeed = await utils.parseFeed(URL);
+
+    return Promise.all(parsedFeed.items.map((item) => {
+      const $ = cheerio.load(item.content);
+
+      const imgSrc = $('img').attr('src');
+      item.content = `<img src="${imgSrc}" class="img-fluid" />`;
+
+      return item;
+    })).then(() => parsedFeed);
+  }
 };
